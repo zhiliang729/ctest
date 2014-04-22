dc091d2da8ae7a124198cfa1bdedca0c3af05491
zhiliang729 <zhiliang729@163.com>
2014-04-22 16:38:15 +0800

    消息队列

---
+++ IPC__messageQueue/IPC__messageQueue/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  IPC__messageQueue
+//
+//  Created by zhangliang on 14-4-22.
+//  Copyright (c) 2014年 com.gooagoo.Gooagoo. All rights reserved.
+//
+
+#include <stdio.h>
+#include <sys/ipc.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
