9949d06861a5346a29a613a132a6022aa8029020
zhiliang729 <zhiliang729@163.com>
2014-04-23 15:10:48 +0800

    信号量

---
+++ IPC__semid/IPC__semid/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  IPC__semid
+//
+//  Created by zhangliang on 14-4-23.
+//  Copyright (c) 2014年 com.gooagoo.Gooagoo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
