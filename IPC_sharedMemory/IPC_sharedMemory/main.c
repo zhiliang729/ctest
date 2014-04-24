37b678348529e68d130d8a6ce34e9afcfe262712
zhiliang729 <zhiliang729@163.com>
2014-04-24 10:06:49 +0800

    共享内存模型

---
+++ IPC_sharedMemory/IPC_sharedMemory/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  IPC_sharedMemory
+//
+//  Created by zhangliang on 14-4-24.
+//  Copyright (c) 2014年 com.gooagoo.Gooagoo. All rights reserved.
+//
+
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
+
