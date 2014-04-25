7630d8978d39f0ce7ec861641d0fe85a0b9d3cb3
zhiliang729 <zhiliang729@163.com>
2014-04-25 19:46:58 +0800

    套接字

--- socketIntro/socketIntro/main.c
+++ socketIntro/socketIntro/main.c
@@ -7,12 +7,34 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    int soc;
+    soc = socket(AF_INET, SOCK_STREAM, 0);
+    soc = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    int nIp;
+    nIp = inet_addr("127.0.0.1");
+    printf("ip:%d\n", nIp);
+    struct in_addr addr;
+    int ret = inet_aton("127.0.0.1", &addr);
+    if (ret == 1) {
+        printf("success : %d\n", addr.s_addr);
+    }
+    char *buf;
+    buf = inet_ntoa(addr);
+    printf("ip: %s\n", buf);
+    struct sockaddr_in addr_in;
+//    bind(soc, , )
+    
     return 0;
 }
 
