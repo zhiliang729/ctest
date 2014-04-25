2bc0bc53826e21fe73f42461db3fad43b004a577
zhiliang729 <zhiliang729@163.com>
2014-04-25 14:34:43 +0800

    套接字

---
+++ socketIntro/socketIntro/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  socketIntro
+//
+//  Created by zhangliang on 14-4-25.
+//  Copyright (c) 2014年 com.gooagoo.Gooagoo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
